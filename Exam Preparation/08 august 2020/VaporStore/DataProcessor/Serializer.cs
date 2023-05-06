6308a494ec811cc0e23da6560b64b72f2d947929
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-05-06 20:12:17 +0300

    Update Serializer.cs

--- Exam Preparation/08 august 2020/VaporStore/DataProcessor/Serializer.cs
+++ Exam Preparation/08 august 2020/VaporStore/DataProcessor/Serializer.cs
@@ -6,12 +6,89 @@
     {
         public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
         {
-            throw new NotImplementedException();
+            var games = context.Genres
+                .Where(g => genreNames.Contains(g.Name))
+                .Select(g => new
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games
+                        .Where(game => game.Purchases.Any())
+                        .Select(game => new
+                        {
+                            Id = game.Id,
+                            Title = game.Name,
+                            Developer = game.Developer.Name,
+                            Tags = string.Join(", ", game.GameTags.Select(x=>x.Tag.Name)),
+                            Players = game.Purchases.Count
+                        })
+                        .OrderByDescending(x => x.Players)
+                        .ThenBy(x => x.Id)
+                        .ToArray(),
+                    TotalPlayers = g.Games.Sum(x => x.Purchases.Count)
+                })
+                .OrderByDescending(x => x.TotalPlayers)
+                .ThenBy(x => x.Id)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(games, Formatting.Indented);
         }
 
         public static string ExportUserPurchasesByType(VaporStoreDbContext context, string purchaseType)
         {
-            throw new NotImplementedException();
+             PurchaseType purchaseTypeEnum = Enum.Parse<PurchaseType>(purchaseType);
+
+            var users = context
+                .Users
+                .Where(u => u.Cards.Any(c => c.Purchases.Any()))
+                .Select(u => new ExportUserDto()
+                {
+                    Username = u.Username,
+                    Purchases = context
+                        .Purchases
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .OrderBy(p => p.Date)
+                        .Select(p => new ExportUserPurchasesDto()
+                        {
+                            Card = p.Card.Number,
+                            Cvc = p.Card.Cvc,
+                            Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                            Game = new ExportGameDto()
+                            {
+                                Name = p.Game.Name,
+                                Genre = p.Game.Genre.Name,
+                                Price = p.Game.Price
+                            }
+                        })
+                        .ToArray(),
+                    TotalSpent = context
+                        .Purchases
+                        .Where(p => p.Card.User.Username == u.Username && p.Type == purchaseTypeEnum)
+                        .Sum(p => p.Game.Price)
+                })
+                .Where(u => u.Purchases.Length > 0)
+                .OrderByDescending(u => u.TotalSpent)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+
+            string rootName = "Users";
+            return Serialize(users, rootName);
+        }
+        
+        //Helper method Xml Export
+        private static string Serialize<T>(T dto, string rootName)
+        {
+            StringBuilder builder = new StringBuilder();
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringWriter writer = new StringWriter(builder);
+            xmlSerializer.Serialize(writer, dto, namespaces);
+
+            return builder.ToString().TrimEnd();
         }
     }
 }
