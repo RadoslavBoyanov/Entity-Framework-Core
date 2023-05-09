f82c56786a6c082f71a65f6e16f7dad441384f28
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-05-09 13:05:31 +0300

    Update Configuration.cs

--- Exam Preparation/06 august 2022/Footballers/Data/Configuration.cs
+++ Exam Preparation/06 august 2022/Footballers/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=DESKTOP-455JGR0\SQLEXPRESS01;Database=FootballersExam;Trusted_Connection=True";
+        public static string ConnectionString = @"Server=;Database=FootballersExam;Trusted_Connection=True";
     }
 }
